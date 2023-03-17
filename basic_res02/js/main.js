7ac5f75fd8ef468596d3c1cb7814f90bbbc673b0
lee <sipossa@naver.com>
2023-03-17 11:50:03 +0900

    20230317

---
+++ basic_res02/js/main.js
@@ -0,0 +1,18 @@
+$(function () {
+
+    var main_slide_option = {
+        arrows: false,
+        vertical: true,
+        dots: true,
+        autoplay: true,
+
+    }
+
+    $('.main_slide').slick(main_slide_option)
+
+
+
+
+
+
+})
