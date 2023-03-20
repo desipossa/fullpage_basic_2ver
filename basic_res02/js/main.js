f7581ee9e0a0b55c5a8ea609b6832699f42a0d66
lee <sipossa@naver.com>
2023-03-20 11:26:10 +0900

    20230320

--- basic_res02/js/main.js
+++ basic_res02/js/main.js
@@ -5,10 +5,50 @@ $(function () {
         vertical: true,
         dots: true,
         autoplay: true,
-
     }
 
     $('.main_slide').slick(main_slide_option)
+
+
+    $('.tab_menu li').on('click', function () {
+        var idx = $(this).index();
+        $('.tab_content li')
+            .eq(idx)
+            .addClass('on')
+            .siblings()
+            .removeClass('on');
+        $(this)
+            .addClass('on')
+            .siblings()
+            .removeClass('on');
+    });
+
+    $('.to_top').on('click', function () {
+        $('html, body').animate({ scrollTop: 0 }, 200)
+    });
+
+    $(window).on('scroll', function () {
+        var sct = $(window).scrollTop();
+
+        sct > 100
+            ?
+            $('.to_top').fadeIn()
+            :
+            $('.to_top').fadeOut()
+    });
+
+
+    $('.family_link').on('change', function () {
+        var lnk = $(this).val();
+        lnk && window.open(lnk);
+    });
+
+    $('.right_banner i').on('click', function () {
+        $(this).toggleClass('on');
+        $('.right_banner').toggleClass('on');
+    })
+
+
 
 
 
